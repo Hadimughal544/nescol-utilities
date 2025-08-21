e52db610a033ce78f01db5ae4b99e5ac927ff592
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-08-21 21:08:11 +0500

    hi

--- src/app/services/[type]/payment-solution/page.js
+++ src/app/services/[type]/payment-solution/page.js
@@ -202,7 +202,7 @@ const handleChange = (index, value, field = null) => {
     if(!validateStep()) return;  
     setLoading(true);
         try{
-           const res =  await fetch("http://nescolutilities.co.uk/api/paymentsolution", {
+           const res =  await fetch("https://nescolutilities.co.uk/api/paymentsolution", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
--- src/app/services/[type]/water/page.js
+++ src/app/services/[type]/water/page.js
@@ -218,7 +218,7 @@ const handleChange = (index, value, field = null) => {
      if (!validateStep()) return;
     setLoading(true);
         try{
-           const res =  await fetch("http://nescolutilities.co.uk/api/water", {
+           const res =  await fetch("https://nescolutilities.co.uk/api/water", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
