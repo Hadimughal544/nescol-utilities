3a9e200c4eb7e972d93ce31c8dd4afd13c561868
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-08-22 20:43:42 +0500

    hllo

--- src/app/services/[type]/broadband/page.js
+++ src/app/services/[type]/broadband/page.js
@@ -229,7 +229,7 @@ const handleChange = (index, value, field = null) => {
     if (!validateStep()) return;
     setLoading(true);
         try{
-           const res =  await fetch("http://31.97.117.214:3001/broadband", {
+           const res =  await fetch("/api/broadband", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
